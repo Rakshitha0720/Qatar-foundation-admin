8fb949c300827942611be3564b4ddf884249d924
Rakshitha0720 <rakshithasrinivas2002@gmail.com>
2026-05-05 01:23:37 +0530

    Fix ModuleNotFoundError by adding sys.path.insert for Vercel imports

--- qatar_foundation_admin/app.py
+++ qatar_foundation_admin/app.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from flask import Flask, send_from_directory, Response
 from flask_login import LoginManager
 from config import Config
